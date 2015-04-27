c1f9e9c06d6f19ce1a82aadb2a77cc30755fd81d
Nikki <nikki@foxleapmoon.com>
2015-04-27 08:22:46 -0600

    Attempt to get component tests working, not quite there yet

--- spec/componentSpec.js
+++ spec/componentSpec.js
@@ -1,7 +1,14 @@
 describe('component',function() {
 
   it ('has a grid', function() {
-    expect(Grid).not.toBeNull();
+  	var grid = Crafty("Grid");
+    expect(grid).not.toBeNull();
+  });
+
+  it ('has a grid with with a w attribute', function() {
+  	var grid = Crafty("Grid");
+  	grid.w = 16;
+    expect(grid.w).toEqual(16);
   });
   
 });
